b55079fa539208992a2502d7b675296fb7e7e925
bitomagira <steve.bitomagira@student.uclouvain.be>
2017-11-29 18:16:57 +0100

    Update Myslot.java

--- Myslot.java
+++ Myslot.java
@@ -1,43 +1,42 @@
 
 public class Myslot 
 {
-	 private static int set;
-	 private static int num;
-	 private static int tag;
-	 private static int flag;
-	 private static int offset;
+	 public int []slot;
 	
 	public Myslot(int a,int b,int c,int d,int e)
 	{
-		this.set=a;
-		this.num=b;
-		this.tag=c;
-		this.flag=d;
-		this.offset=e;
+		slot=new int[3];
+		slot[0]=c;
+		slot[1]=d;
+		slot[2]=e;
 	}
 
 	public  int getTag() {
-		return tag;
+		return slot[0];
 	}
 
-	public  void setTag(int tag) {
-		this.tag = tag;
+	public  void setTag(int t) {
+		slot[0]= t;
 	}
 
 	public  int getFlag() {
-		return flag;
+		return slot[1];
 	}
 
-	public void setFlag(int flag) {
-		this.flag = flag;
+	public void setFlag(int f) {
+		slot[1] = f;
 	}
 
 	public  int getOffset() {
-		return offset;
+		return slot[2];
 	}
 
-	public  void setOffset(int offset) {
-		this.offset = offset;
+	public  void setOffset(int a) {
+		slot[2] = a;
+	}
+	
+	public String toString() {
+		return "Tag: "+slot[0]+" offset: "+slot[1]+" Flag: "+slot[2];
 	}
 	
 	
