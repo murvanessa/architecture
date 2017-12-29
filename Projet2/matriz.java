948c2a7115f92cae6e8b17d318ac4589fea95d73
bitomagira <steve.bitomagira@student.uclouvain.be>
2017-12-29 17:44:19 +0100

    Update matriz.java

--- Projet2/matriz.java
+++ Projet2/matriz.java
@@ -1,26 +1,20 @@
 import java.util.*;
 
 public class Matriz {
-	
-	private double matriz[][];
-	private Random random;
-	
-	public Matriz() {
-		matriz = new double [4][4];
-		random = new Random();
-	}
-	
-	public void llnearMatriz() {
-		try{
-			for(int i =0 ; i<matriz.length; i++) {
-				for(int j = 0; j<matriz.length; j++) {
-					matriz[i][j] = random.nextInt(20);
-				}
-			}
-			}catch(Exception e) {
-			System.out.println(e.getMessage());
+	int num=0;
+	private int matrix[][];	
+	public void llnearMatriz() 
+	{
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				matrix[i][j]=num;
+				num+=1;
 			}
 		}
+	}
 	
 	public void imprimerMatrix() {
 		for(int i =0 ; i<matriz.length; i++) {
