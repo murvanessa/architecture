3e0fc523de0763dac171cf3aabffd5b60fffbe50
murvanessa <32261166+murvanessa@users.noreply.github.com>
2017-12-29 20:04:57 +0100

    Update MatrixClient.java

--- Projet2/MatrixClient.java
+++ Projet2/MatrixClient.java
@@ -1,8 +1,10 @@
-import java.util.Random;
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class MatrixClient {
+	
+	static String V = new String();
 	
 	public static void main(String args[]) {
 		String hostname = "localhost";
@@ -16,12 +18,51 @@ public class MatrixClient {
 		}
 		try {
 			Socket socket = new Socket(hostname, port);
-			Matriz m = new Matriz();
-			m.llnearMatriz();
 			
 			BufferedWriter writer = new BufferedWriter(
 					new OutputStreamWriter(socket.getOutputStream()));
-			writer.write("m.imprimerMatrix();,2");
+			int [][] A;
+			int k;
+			int num=1;
+			int puissance=0;
+			Scanner sc = new Scanner(System.in);
+			System.out.printf("entrer le row : ");
+			k = sc.nextInt();
+			A=new int[k][k];
+				for(int i =0 ; i<k; i++) 
+				{
+					for(int j = 0; j<k; j++) 
+					{
+						
+						A[i][j] = num;
+						num+=1;
+					}
+				}
+				
+				for(int i =0 ; i<A.length; i++) {
+					for(int j = 0; j<A.length; j++) {
+						System.out.print(A[i][j]+"\t");
+					}
+					System.out.println();
+				}	
+			for(int i=0; i < A.length; i++) 
+			{
+				for(int j=0; j < A.length; j++) 
+				{
+					V+= String.valueOf(A[i][j]);
+					V+=" ";
+				}
+				V+="/";
+			}
+			
+			String[] B= V.split("/");
+			System.out.println(V);
+			Scanner sc1 = new Scanner(System.in);
+			System.out.printf("entrer la puissance : ");
+			puissance = sc1.nextInt();
+			V+=puissance;
+	
+			writer.write(V);
 			writer.newLine();
 			writer.flush();
 			BufferedReader reader = new BufferedReader(
